9f427af0f9f74deb9f145006bf973838b0fc6149
Daniel Grecco <danielalbertogrecco@gmail.com>
2023-02-10 12:34:21 -0300

    correcion dir

---
+++ 0022_cmpLH/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    Compo1Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 0024_dirNgIf/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '0024_dirNgIf';
+  grupo1=false;
+  grupo2=false;
+  subgrupo11=false;
+  subgrupo12=false;
+  subgrupo21=false;
+  subgrupo22=false;
+  
+}
